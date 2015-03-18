abfbae738274ccd4ee903e397e54a33f267462ec
mexisme <wildjim+github.com@kiwinet.org>
2015-03-18 22:10:33 +1300

    Missing requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+require 'simplecov'
+SimpleCov.start
+
+#####
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'exception_notifier/squash'
+
+require 'exception_notification'
+require 'exception_notifier/squash_notifier'
